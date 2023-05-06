87afe5dfce76d419354516870a14a9123de6510a
Bass Neel <maps.bassneel@gmail.com>
2023-05-06 01:13:29 -0400

    whoops

--- Home.py
+++ Home.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
 st.title('Welcome to BassMap')
 
 st.sidebar.info('Contact')
-st.sidebar.markdown('[Twitter](https://twitter.com/giswqs)')
+st.sidebar.markdown('[LinkedIn](https://www.linkedin.com/in/bassneel/)')
