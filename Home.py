48be8489c2a957fda7472cc8b3b0e4a31793fd9b
Bass Neel <maps.bassneel@gmail.com>
2023-05-06 01:11:02 -0400

    trying to fix this

--- Home.py
+++ Home.py
@@ -1,3 +1,8 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('BassMap')
+st.title('Welcome to BassMap')
+
+st.sidebar.info('Contact')
+st.sidebar.markdown('[Twitter](https://twitter.com/giswqs)')
