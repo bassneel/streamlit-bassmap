54e768cc63e06ac844a49199d8c7a9675c3fa6b9
Bass Neel <116589974+bassneel@users.noreply.github.com>
2023-05-11 00:19:09 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,43 @@
 import streamlit as st
 
-st.title('Welcome to BassMap')
+st.title('BassMap')
 
-st.sidebar.info('Contact')
-st.sidebar.markdown('[LinkedIn](https://www.linkedin.com/in/bassneel/)')
+st.markdown("""
+    BassMap is an intuitive Python Package that allows the user to display a variety of features on an iPyLeaflet and Folium map display. In addition the Landsat 8 Tools allows the user to create a variety of multispectral composites using local GeoTIFFs from Landsat 8 spectral bands
+""")
+
+st.subheader('Features')
+
+st.markdown("""
+-   [Landsat 8 Tools](https://bassneel.github.io/bassmap/examples/Landsat_8_Tools/)
+    -   Multispectral Composites
+        -   Create True Color Composite
+        -   Create False Color (Buildings) Composite
+        -   Create Color Infrared (Vegetation) Composite
+        -   Create Healthy Vegetation Composite
+    -   Spectral Indicies
+        -   Create Normalized Difference Moisture Index (NDMI) Composite
+        -   Create Normalized Difference Vegetation Index (NDVI) Composite
+
+-   With IPyLeaflet ([Mapomatic](https://bassneel.github.io/bassmap/examples/Mapomatic/))
+    -   Change basemap
+    -   Display shapefiles
+    -   Display GeoJSON files
+    -   Display vector files
+    -   Display Cloud Optimized GeoTIFFs
+-   With Folium ([Foliumatic](https://bassneel.github.io/bassmap/examples/Foliumatic/))
+    -   Change basemap
+    -   Display shapefiles
+    -   Display GeoJSON files
+    -   Display vector files
+""")
+
+st.subheader('Demo')
+
+# Add your demo code here
+
+st.subheader('Credits')
+
+st.markdown("""
+This package was created with [Cookiecutter](https://github.com/cookiecutter/cookiecutter) and the [giswqs/pypackage](https://github.com/giswqs/pypackage) project template.
+""")
