f550d829a5eba63f853503bd273b752fb73a6feb
Bass Neel <116589974+bassneel@users.noreply.github.com>
2023-05-11 11:01:43 -0400

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 st.title('About')
 
-st.write('Information about BassMap')
+st.header('Information about BassMap')
+
+st.write('BassMap is an intuitive Python Package that allows the user to display a variety of features on an iPyLeaflet and Folium map display. In addition the Landsat 8 Tools allows the user to create a variety of multispectral composites using local GeoTIFFs from Landsat 8 spectral bands')
